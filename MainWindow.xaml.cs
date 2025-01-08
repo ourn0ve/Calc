b23bb2f0d086b9f362a423ffb5d6713a09b0d859
our_n0ve1 <moiseenkoalisa17@gmail.com>
2025-01-09 01:01:18 +0700

    Add equals logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calc
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private double num1, num2, result;
+        private string operation = string.Empty;
+        private void OnOperationClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            operation = btn.Content.ToString();
+        }
+
+        private void OnEqualsClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtInput1.Text, out num1) && double.TryParse(txtInput2.Text, out num2))
+            {
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                            MessageBox.Show("Ошибка: деление на ноль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                    default:
+                        MessageBox.Show("Выберите операцию", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                }
+
+                txtResult.Text = result.ToString();
+            }
+            else
+                MessageBox.Show("Ошибка: некорректный ввод", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private void OnClearClick(object sender, RoutedEventArgs e)
+        {
+            txtInput1.Clear();
+            txtInput2.Clear();
+            txtResult.Text = string.Empty;
         }
     }
 }
